4e50071fb00d63ae23ba6639b2d51dde66eef737
austin <austinchang4@gmail.com>
2021-11-10 13:19:20 +0800

    init Crawler on app start

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import './pre-start'; // Must be the first import
 import app from '@server';
 import logger from '@shared/Logger';
+import { C } from '@entities/Crawler';
+
+// init crawler
+// top level await not supported yet
+(async () => {
+    console.log("Initialising Crawler ...");
+    await C.init().catch(err => {
+        console.error(err.message);
+        process.exit(1);
+    });
+    console.log("> Successfully initialised Crawler <");
+})();
 
 
 // Start the server
