64ad56a87cdb4ce74014b43b16cbe85833143ea7
austin <austinchang4@gmail.com>
2021-11-11 18:44:34 +0800

    build ignore env file error

--- src/pre-start/index.ts
+++ src/pre-start/index.ts
@@ -10,20 +10,25 @@ import commandLineArgs from 'command-line-args';
 
 
 (() => {
-    // Setup command line options
-    const options = commandLineArgs([
-        {
-            name: 'env',
-            alias: 'e',
-            defaultValue: 'development',
-            type: String,
-        },
-    ]);
-    // Set the env file
-    const result2 = dotenv.config({
-        path: path.join(__dirname, `env/${options.env}.env`),
-    });
-    if (result2.error) {
-        throw result2.error;
+    try {
+        // Setup command line options
+        const options = commandLineArgs([
+            {
+                name: 'env',
+                alias: 'e',
+                defaultValue: 'development',
+                type: String,
+            },
+        ]);
+        // Set the env file
+        const result2 = dotenv.config({
+            path: path.join(__dirname, `env/${options.env}.env`),
+        });
+        if (result2.error) {
+            throw result2.error;
+        }
+    } catch (err) {
+        console.error(`ERR: ${(err as Error).message}`);
     }
+
 })();
